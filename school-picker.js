147778a58f0acb20c3981efb40ecd9befc1b134b
Addison McCaleb <addison@mediahound.com>
2017-10-21 01:40:55 -0700

    Color Change + JS
    Color change persists across the site now. Added a new JS file for those controls. Also made the songs play when you hover on that button. Added Overview page

---
+++ school-picker.js
@@ -0,0 +1,57 @@
+$( document ).ready(function() {
+  var teamstatus = localStorage.getItem('team');
+  if(teamstatus=='usc'){
+
+      //Set Nav Styles
+      $( ".navbar" ).addClass( 'usc' );
+      $( ".navbar" ).removeClass( 'ucla' );
+
+      //Set Footer Styles
+      $( "footer" ).addClass( 'usc' );
+      $( "footer" ).removeClass( 'ucla' );
+      $("footer *").css({'color': 'white'});
+  }
+  else if (teamstatus=='ucla'){
+
+      //Set Nav Styles
+      $( ".navbar" ).addClass( 'ucla' );
+      $( ".navbar" ).removeClass( 'usc' );
+
+      //Set Footer Styles
+      $( "footer" ).addClass( 'ucla' );
+      $( "footer" ).removeClass( 'usc' );
+      $("footer *").css({'color': 'white'});
+  }
+
+  $('.button-usc')
+    .click( function() {
+
+      //Set Nav Styles
+      $( ".navbar" ).addClass( 'usc' );
+      $( ".navbar" ).removeClass( 'ucla' );
+
+      //Set Footer Styles
+      $( "footer" ).addClass( 'usc' );
+      $( "footer" ).removeClass( 'ucla' );
+      $("footer *").css({'color': 'white'});
+
+      // set the value upon clicking
+      window.localStorage.setItem('team', 'usc')
+    });
+
+  $('.button-ucla')
+    .click( function() {
+
+      //Set Nav Styles
+      $( ".navbar" ).addClass( 'ucla' );
+      $( ".navbar" ).removeClass( 'usc' );
+
+      //Set Nave Styles
+      $( "footer" ).addClass( 'ucla' );
+      $( "footer" ).removeClass( 'usc' );
+      $("footer *").css({'color': 'white'});
+
+      // store the value upon clicking
+      window.localStorage.setItem('team', 'ucla')
+      });
+});
