a1cf285dcd604c4ac000b8f71185315cbf2931dd
nicolela <32502656+nicolela@users.noreply.github.com>
2017-10-22 22:51:46 -0700

    Fixed CSS & Added Content To Overviews / Added Crosstown Cup page

--- rivalry.js
+++ rivalry.js
@@ -4,17 +4,20 @@ $(document).ready(function(){
         $('#crosstown-section').hide();
         $('#victory-section').hide();
 		$("#two:before").css("height", "630%");
+		$("#rivalryheader").css("background-image", "url(images/top-1600.svg), url(images/overlay.png), url(images/oldticket.jpg)");
     });
     $("#crosstown-button").click(function(){
         $('#timeline-section').hide();
         $('#crosstown-section').show();
         $('#victory-section').hide();
 		$("#two:before").css("height", "100%");
+		$("#rivalryheader").css("background-image", "url(images/top-1600.svg), url(images/overlay2.png), url(images/crosstowncup.jpg)");
     });
     $("#victory-button").click(function(){
         $('#timeline-section').hide();
         $('#crosstown-section').hide();
         $('#victory-section').show();
 		$("#two:before").css("height", "100%");
+		$("#rivalryheader").css("background-image", "url(images/top-1600.svg), url(images/overlay.png), url(images/uclavictorybell.jpg)");
     });
 });
