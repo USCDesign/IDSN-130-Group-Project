f488475e1cd85c1214d1d184568fb08820996fd9
Addison McCaleb <addison@mediahound.com>
2017-10-23 14:35:20 -0700

    Mobile Reponsive Nav
    Made nav mobile responsive with hamburger nav

--- rivalry-nav.js
+++ rivalry-nav.js
@@ -26,28 +26,3 @@ $(document).ready(function() {
 
 
 });
-    //
-    // if (location.hash == "#timeline-page"){
-    //     $("#timeline-section").show();
-    //     $("#crosstown-section").hide();
-    //     $("#victory-section").hide();
-    //     $("#two:before").css("height", "630%");
-    // });
-    // else if (location.hash == "#crosstown"){
-    //     $("#timeline-section").hide();
-    //     $("#crosstown-section").show();
-    //     $("#victory-section").hide();
-    //     $("#two:before").css("height", "100%");
-    // });
-    // else if (location.hash == "#victory"){
-    //     $("#timeline-section").hide();
-    //     $("#crosstown-section").hide();
-    //     $("#victory-section").show();
-    //     $("#two:before").css("height", "100%");
-    // });
-    // else {
-    //     $("#timeline-section").hide();
-    //     $("#crosstown-section").hide();
-    //     $("#victory-section").show();
-    //     $("#two:before").css("height", "100%");
-    // });
